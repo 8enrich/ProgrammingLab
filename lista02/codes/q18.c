541b342dbc86e14c3f40e5da7cfb3ff5daaf6c5b
henriqueftcunha@gmail.com <henriqueftcunha@gmail.com>
2023-09-04 21:10:18 -0300

    Update question 18

--- lista02/codes/q18.c
+++ lista02/codes/q18.c
@@ -2,13 +2,9 @@
 
 int main(){
 	
-	unsigned long long int sum = 0;
-	unsigned long long int num = 1;
+	unsigned long long int sum = 1;
 
-	for(int i = 0; i < 64; i++){
-		num *= 2;
-		sum += num;
-	}
+	sum = (sum << 63);
 	
 	printf("O somatório da quantidade de grãos de trigo é: %llu\n",sum);
 	return 0;
