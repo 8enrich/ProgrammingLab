91b3a389b3d58a42d0f47d60f430acb881c15333
henriqueftcunha@gmail.com <henriqueftcunha@gmail.com>
2023-09-06 18:56:47 -0300

    Update question 27

--- lista02/codes/q27.c
+++ lista02/codes/q27.c
@@ -1,29 +1,36 @@
 #include <stdio.h>
 
+int bigger_num(int x, int y){
+	return (y > x) * y + (y <= x) * x;
+}
+
+int smaller_num(int x, int y){
+	return (y < x) * y + (y >= x) * x;
+}
 int main(){
 
 	int num = 0;
 	int bigger = 0, lower = 0;
+	int i = 0;
 
 	while(1){
 		printf("Digite um número inteiro(ou um par negativo para sair): ");
 		scanf("%d", &num);
 
-		if(!(bigger) & !(lower)){
+		if(!(num % 2) & num < 0)
+                        break;
+
+		if(!(i)){
 			bigger = num;
 			lower = num;
+			i++;
 		}
 
-		if(num > bigger)
-			bigger = num;
+		bigger = bigger_num(num, bigger);
 
-		if(num < lower)
-			lower = num;
-		
-		if(!(num % 2) & num < 0)
-			break;
+		lower = smaller_num(num, lower);
 
-		printf("O produto do maior pelo menor número apresentado é: %.2f\n", (float)bigger/lower);
+		printf("O produto de %d por %d é: %d\n", bigger, lower, bigger * lower);
 
 	}
 
